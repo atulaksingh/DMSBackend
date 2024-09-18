84d5686a29d91b93bb7396c1bcffb7800fb78c73
vaishnavi <vaishnavitalari1404@gmail.com>
2024-09-18 15:21:09 +0530

    admin done

--- DMS/api/admin.py
+++ DMS/api/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from api.models import *
 
 # Register your models here.
-admin.site.register(Client)
 
-# class FileInline(admin.StackedInline):
-#     model = File
-#     extra = 1
+class FileInline(admin.StackedInline):
+    model = File
+    extra = 1
 
-# class AttachmentAdmin(admin.ModelAdmin):
-#     inlines = [FileInline]
-#     list_display = ('file_name', 'status', 'client')
-#     search_fields = ('file_name', 'client_name')
+class FileAdmin(admin.ModelAdmin):
+    inlines = [FileInline]
+    # list_display = ('file_name', 'status', 'client')
+    # search_fields = ('file_name', 'client_name')
 
+admin.site.register(Client, FileAdmin)
 # admin.site.register(Attachment, AttachmentAdmin)
 admin.site.register(File)
 admin.site.register(CompanyDocument)
