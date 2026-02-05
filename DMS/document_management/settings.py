10b17e698bdb42c502a7f18f718b772d32d7ffc4
Abhiraj Das <90388893+abhirajdas06@users.noreply.github.com>
2026-02-05 13:20:11 +0530

    Change database engine to PostgreSQL
    Updated database settings to use PostgreSQL instead of SQLite.

--- DMS/document_management/settings.py
+++ DMS/document_management/settings.py
@@ -96,12 +96,27 @@ WSGI_APPLICATION = 'document_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dms',
+        'USER': 'dms_user',
+        'PASSWORD': 'g0Lv3Fv90Kbn',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 # manually added
 REST_FRAMEWORK = {
